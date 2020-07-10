ea52526c4ca43211eb972ee9b427ab18781f0a66
Rag <gian.rathgeb@brel.ch>
2020-07-10 11:57:56 +0200

    first hard challange

---
+++ hard/Does_the_Cargo_Fit_Part-2/main.py
@@ -0,0 +1,40 @@
+# Challange: https://edabit.com/challenge/zzibM5MaxDNvQCrEk
+
+# Input: will_fit(["M", "L", "L", "M"], [56, 62, 84, 90]) --> True
+
+def will_fit(holds, cargo):
+	spaces = []
+	space_left = []
+	cargo_fits = []
+	next_cargo = False
+	for h in holds:
+		if h == "S": spaces.append(50)
+		elif h == "M": spaces.append(100)
+		elif h == "L": spaces.append(200)
+	spaces.sort(reverse=True)
+	cargo.sort(reverse=True)
+	for x in cargo:
+		for y in spaces:
+			if x <= y:
+				space_left.append(y - x)
+				spaces.remove(y)
+				next_cargo = True
+				break
+		if next_cargo:
+			next_cargo = False
+			continue
+		for z in space_left:
+			if x <= z:
+				space_left.append(z - x)
+				try:
+					spaces.remove(z)
+				except ValueError:
+					# Do nothing, cause nothing in Spaces
+					pass
+				next_cargo = True
+				break
+		if next_cargo:
+			next_cargo = False
+			continue
+		return False
+	return True
---
+++ hard/Does_the_Cargo_Fit_Part-2/readme.md
@@ -0,0 +1,6 @@
+Does the Cargo Fit? (Part 2)
+A ship has to transport cargo from one place to another, while picking up cargo along the way. Given the total amount of cargo and the types of cargo holds in the ship as lists, create a function that returns True if each weight of cargo can fit in one hold, and False if it can't.
+
+* "S" means 50 cargo space.
+* "M" means 100 cargo space.
+* "L" means 200 cargo space.
